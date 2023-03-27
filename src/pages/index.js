cfda422afcbd5579d960f0b0b46af30cb1eb1bb1
Celine Verschueren <celineverschueren@MacBook-Pro-van-Celine-2.local>
2023-03-27 11:12:33 +0200

    viewed video next fetch and installed everything

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,19 @@
 import styles from '@/styles/Home.module.css'
+import {userState} from 'react';
+import useNetwork from '@/data/network'
 
 export default function Home() {
+  const { network, isLoading, isError } = useNetwork()
+ 
+  if (isLoading) return <div>Loading...</div>
+  if (isError) return<div>Error</div>
+
+  const stations = network.stations;
+ 
   return (
-    <div></div>
+    <div>
+      {stations.map(station => <p key={station.id}>{station.name}</p>)}
+    </div>
   );
 };
 
