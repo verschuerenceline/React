84c5475c1b71b9fa91d9a10b80a3bc962c2a38e5
Celine Verschueren <celineverschueren@MacBook-Pro-van-Celine-2.local>
2023-05-22 11:39:15 +0200

    menu in orde

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -9,46 +9,43 @@ export default function Layout({ children }) {
     setIsMenuOpen(!isMenuOpen);
   };
 
+  const handleCloseMenu = () => {
+    setIsMenuOpen(false);
+  };
+
   return (
     <div>
-  <nav className={styles.navbar}>
-    <div className={styles.navbar}></div>
-    <button
-      className={`${styles["navbar-toggler"]} ${isMenuOpen ? styles["open"] : ""}`}
-      onClick={handleMenuToggle}
-    >
-      <span></span>
-      <span></span>
-      <span></span>
-      
-    </button>
-    <div className={`${styles["navbar-nav"]} ${isMenuOpen ? styles["open"] : ""}`}>
-      <div className={styles["nav-item"]}>
-        <span>Menu</span>
-        <ul className={styles["dropdown-menu"]}>
+      <nav className={styles.navbar}>
 
-          <li>
-            <Link href="/">Home</Link>
-          </li>
-      
-          <li>
-            <Link href="/about">About</Link>
-          </li>
-
-          <li>
-            <Link href="/stations">Stations</Link>
-          </li>
-
-          <li>
-            <Link href="/locaties">Locaties</Link>
-          </li>
-          
-        </ul>
-      </div>
+        <div className={`${styles["navbar-nav"]} ${isMenuOpen ? styles["open"] : ""}`}>
+          <div className={styles["nav-item"]}>
+            <span>Menu</span>
+            <ul className={styles["dropdown-menu"]}>
+              <li>
+                <Link onClick={handleMenuToggle} href="/">Home</Link>
+              </li>
+              <li>
+                <Link onClick={handleMenuToggle} href="/about">About</Link>
+              </li>
+              <li>
+                <Link onClick={handleMenuToggle} href="/stations">Stations</Link>
+              </li>
+              <li>
+                <Link onClick={handleMenuToggle} href="/locaties">Locaties</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <button
+          className={`${styles["navbar-toggler"]} ${isMenuOpen ? styles["open"] : ""}`}
+          onClick={handleMenuToggle}
+        >
+          <span></span>
+          <span></span>
+          <span></span>
+        </button>
+      </nav>
+      <main>{children}</main>
     </div>
-  </nav>
-  <main>{children}</main>
-</div>
-
   );
 }
