f2316276a702808ccf86d015a0a1c5e588d079bf
Celine Verschueren <celineverschueren@MacBook-Pro-van-Celine-2.local>
2023-05-26 13:16:00 +0200

    test

--- src/pages/about.js
+++ src/pages/about.js
@@ -18,15 +18,16 @@ export default function About() {
     <div className={styles.banner}>
       <h1 className={styles.title}>OVER {network.name}</h1>
     </div>
-    <div>
 
-      <p className={styles.text}>
+    <div>
+      <p className={styles.textabout}>
         TripTrek is dé app voor toeristen die Antwerpen willen verkennen. Of je nu op zoek bent naar de beste cafés, de mooiste uitzichten of de verborgen pareltjes van de stad, TripTrek wijst je de weg. Met deze app vind je makkelijk de beste routes.
         Of je nu een fiets wilt huren bij een van onze populaire stations of een van de voorgestelde routes wilt volgen, TripTrek maakt het allemaal mogelijk. Onze app biedt een gebruiksvriendelijke interface en betrouwbare informatie, zodat je optimaal kunt genieten van je verblijf in Antwerpen. Ontdek de stad op jouw manier met Triptrek!
       </p>
     </div>
+
     <div className={styles.footer}>
-        <img src="/assets/footer.over.png" alt="Footer Image" className={styles.footerImage} width={520} height={480}  />
+        <img src="/assets/footer.over.png" alt="Footer Image" className={styles.footerImage} width={420} height={400}  />
       </div>
   </div>   
 
