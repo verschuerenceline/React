af437266f51fefd8fa780fbf06e24d8137a58b36
Celine Verschueren <celineverschueren@MacBook-Pro-van-Celine-2.local>
2023-05-27 14:15:27 +0200

    http naar https

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Link from 'next/link';
-import styles from '@/styles/Footer.module.css'; // importeer CSS-modules
+import styles from '@/styles/Footer.module.css'; 
 
 
 const Footer = () => {
